17d90b3b9fffdd92d42a1c30f0f08a1da5c73580
yuyuehill <yuyuehill@gmail.com>
2015-06-24 20:57:38 +0800

    Add parameter for root path

--- converter/main.go
+++ converter/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
+    "flag"
     "fmt"
     "os"
     "path/filepath"
     
 )
 
-
-
+var data_root_path = flag.String("root",".","the root path of csv files")
 
 
 func listFiles(path string, f os.FileInfo, err error) error {
@@ -43,8 +43,26 @@ func listFiles(path string, f os.FileInfo, err error) error {
   convert according to predefined rules
 */
 func main(){
-  root_folder := "/Users/hill/oil/test"
-  e := filepath.Walk(root_folder, listFiles)
+  if len(os.Args) != 3 {
+    fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+    flag.PrintDefaults()
+    return
+  }
+  flag.Parse()
+  
+  fi, err := os.Stat(*data_root_path)
+  if err == nil {
+     if ! fi.IsDir() {
+        fmt.Printf(" %s is not a valid path\n", *data_root_path)
+        return
+     }
+  }else{
+     fmt.Printf(" failed to open %s , Error is \"%s\" \n", *data_root_path, err)
+     return 
+  }
+    
+  
+  e := filepath.Walk(*data_root_path, listFiles)
   if e != nil {
       fmt.Println("failed to parse the root folder")  
   }
